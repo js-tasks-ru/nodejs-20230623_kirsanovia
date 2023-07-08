32b268ccd45e71a29b490183359b847aeefe2644
Ilya Kirsanov <kirsanovia@yandex.ru>
2023-07-08 12:58:26 +0400

    streams basics: tasks 1 and 2

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,20 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit;
+    this.size = 0;
   }
 
-  _transform(chunk, encoding, callback) {
+  _transform(chunk, _encoding, callback) {
+    this.size += chunk.length;
+    const data = chunk.toString();
 
+    if (this.size > this.limit) {
+      return callback(new LimitExceededError());
+    } else {
+      callback(null, data);
+    }
   }
 }
 
--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,32 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.remainder = '';
   }
 
-  _transform(chunk, encoding, callback) {
+  _transform(chunk, _encoding, done) {
+    const chunkStringified = this.remainder + chunk.toString();
+
+    let line = '';
+    for (const character of chunkStringified.split('')) {
+      if (character === os.EOL) {
+        this.push(line);
+        line = '';
+        continue;
+      }
+
+      line += character;
+    }
+
+    this.remainder = line;
+    done();
   }
 
-  _flush(callback) {
+  _flush(done) {
+    if (this.remainder) {
+      this.push(this.remainder);
+    }
+    done();
   }
 }
 
