3bf1d91c474c65ed8ab7739fcf967ec63a2b9ef1
Ilya Kirsanov <kirsanovia@yandex.ru>
2023-07-25 15:57:02 +0400

    mongodb: the third task

--- 06-mongodb-mongoose/03-search/controllers/products.js
+++ 06-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,13 @@
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+  const {query} = ctx.query;
+  if (!query) return next();
+
+  return Product.find( {$text: {$search: query}} )
+      .then((data) => {
+        ctx.body = {products: (data || []).map(mapProduct)};
+      })
+      .catch((_) => ctx.throw(500, 'Unexpected error occured'));
 };
