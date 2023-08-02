044409c4e44fecf275bf942fd5a32117713a2386
Ilya Kirsanov <kirsanovia@yandex.ru>
2023-08-03 00:24:04 +0400

    07-authentication-02-auth: implement authentication via github and vk

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,18 @@
+const User = require('../../models/User');
+
 module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+  if (!email) return done(null, false, 'Не указан email');
+
+  return User.findOne({email})
+      .then((currentUser) => {
+        if (currentUser) return done(null, currentUser);
+        const user = {email, displayName};
+
+        return User.create(user)
+            .then(() => done(null, user))
+            .catch((e) => done(e, false, e.errors.email.message));
+      })
+      .catch((_) => {
+        done(null, false, 'Unexpected error occured');
+      });
 };
