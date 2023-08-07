9ad9478b3c3b4bac5f38d92ffe73c5a6ba3f5803
Ilya Kirsanov <kirsanovia@yandex.ru>
2023-08-08 00:06:03 +0400

    07-authentication-03-session: implement session support

--- 07-authentication/03-sessions/app.js
+++ 07-authentication/03-sessions/app.js
@@ -32,6 +32,9 @@ app.use(async (ctx, next) => {
 app.use((ctx, next) => {
   ctx.login = async function(user) {
     const token = uuid();
+    await Session.findOneAndUpdate({user: user.id}, {token, user, lastVisit: new Date()},
+        {upsert: true},
+    );
 
     return token;
   };
@@ -45,6 +48,16 @@ router.use(async (ctx, next) => {
   const header = ctx.request.get('Authorization');
   if (!header) return next();
 
+  const token = header.split(' ')[1];
+  if (!token) return next();
+
+  const session = await Session
+      .findOneAndUpdate({token}, {lastVisit: new Date()}, {new: true})
+      .populate('user');
+
+  if (!session) ctx.throw(401, 'Неверный аутентификационный токен');
+  ctx.user = session.user;
+
   return next();
 });
 
@@ -53,7 +66,7 @@ router.post('/login', login);
 router.get('/oauth/:provider', oauth);
 router.post('/oauth_callback', handleMongooseValidationError, oauthCallback);
 
-router.get('/me', me);
+router.get('/me', mustBeAuthenticated, me);
 
 app.use(router.routes());
 
